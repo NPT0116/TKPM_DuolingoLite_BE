62e6ecc3b0373c1b2b2b97c00f9265f1424d92ea
Nguyen Phuc Thanh <139705216+NPT0116@users.noreply.github.com>
2025-02-22 22:13:55 +0700

    Duolite 17 speech to text api service (#8)
    * DUOLITE-13 finish and ready to test questionWord api
    
    * DUOLITE-17 finish api for testing upload audio file to text

---
+++ src/WebApi/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Net;
+using System.Text.Json;
+
+namespace WebApi.Middlewares;
+
+ public class GlobalExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            // Bạn có thể log exception tại đây nếu cần
+            var response = new { error = exception.Message };
+            var result = JsonSerializer.Serialize(response);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            return context.Response.WriteAsync(result);
+        }
+    }
