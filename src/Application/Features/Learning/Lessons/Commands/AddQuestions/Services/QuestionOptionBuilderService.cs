d2fcac2578b3ee46838fd4ff05a295462c232bc4
pinkWar123 <nquan003@gmail.com>
2025-03-27 12:05:37 +0700

    DUOLITE-62 Update logic of question option builder service to create audio for option if it is in English

--- src/Application/Features/Learning/Lessons/Commands/AddQuestions/Services/QuestionOptionBuilderService.cs
+++ src/Application/Features/Learning/Lessons/Commands/AddQuestions/Services/QuestionOptionBuilderService.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Application.Common.Interface;
-using Application.Interface;
-using Domain.Entities.Learning.Questions;
+using Application.Features.Media.Commands.Upload;
 using Domain.Entities.Learning.Questions.Enums;
 using Domain.Entities.Learning.Questions.Options;
 using Domain.Entities.Learning.Questions.QuestionOptions;
@@ -12,7 +7,7 @@ using Domain.Entities.Learning.Questions.QuestionOptions.Factory;
 using Domain.Entities.Learning.Questions.QuestionOptions.Validator;
 using Domain.Entities.Learning.Words.Enums;
 using Domain.Repositories;
-using Microsoft.VisualBasic;
+using MediatR;
 using SharedKernel;
 using LearningQuestion = Domain.Entities.Learning.Questions.Question;
 namespace Application.Features.Learning.Lessons.Commands.AddQuestions.Services
@@ -23,18 +18,27 @@ namespace Application.Features.Learning.Lessons.Commands.AddQuestions.Services
         private readonly IQuestionOptionValidator _validator;
         private readonly IOptionRepository _optionRepository;
         private readonly IWordService _wordService;
+        private readonly IMediaRepository _mediaRepository;
+        private readonly ITextToSpeechService _textToSpeechService;
+        private readonly IMediator _mediator;
 
         public QuestionOptionBuilderService(
             IQuestionOptionFactory factory,
             IQuestionOptionValidator validator,
             IOptionRepository optionRepository,
-            IWordService wordService
+            IWordService wordService,
+            IMediaRepository mediaRepository,
+            ITextToSpeechService textToSpeechService,
+            IMediator mediator
         )
         {
             _factory = factory;
             _validator = validator;
             _optionRepository = optionRepository;
             _wordService = wordService;
+            _mediaRepository = mediaRepository;
+            _textToSpeechService = textToSpeechService;
+            _mediator = mediator;
         }
         public async Task<Result<List<QuestionOptionBase>>> BuildQuestionOptions(
             List<OptionBaseDto> options, LearningQuestion question, 
@@ -115,11 +119,43 @@ namespace Application.Features.Learning.Lessons.Commands.AddQuestions.Services
                 var option = await _optionRepository.FindOptionThatExactlyMatches(word, targetLanguage);
                 if(option == null)
                 {
+                    Domain.Entities.Media.Media? audio = null;
+                    if(targetLanguage == Language.en)
+                    {
+                        var wordDefinitions = await _wordService.GetWordDefinition(word);
+                        foreach(var wordDefinition in wordDefinitions)
+                        {
+                            var phonetics = wordDefinition.Phonetics;
+                            var withAudio = phonetics.FirstOrDefault(p => !string.IsNullOrEmpty(p.Audio));
+                            var pAudio = withAudio!.Audio;
+                            if(withAudio != null)
+                            {
+                                var uploadedMedia = await _mediaRepository.UploadFileAsync(
+                                pAudio!, pAudio!, 
+                                Domain.Entities.Media.Enums.MediaType.Audio, 10, 
+                                DateTime.UtcNow, DateTime.UtcNow, 
+                                pAudio!, CancellationToken.None);
+
+                                if(uploadedMedia.IsSuccess) audio = uploadedMedia.Value;
+                                break;
+                            }
+                        }
+
+                        if(audio == null)
+                        {
+                            var bytes = _textToSpeechService.GenerateAudioFileFromText(word);
+                            var uploadedRequest = new MediaUploadRequest("question", bytes, word, "audio/mp3");
+                            var uploadCommand = new MediaUploadCommand(uploadedRequest);
+                            var uploadResult = await _mediator.Send(uploadCommand);
+                            if(uploadResult.IsSuccess) audio = uploadResult.Value;
+                        }
+                    }
                     var newOption = Option.Create(
                         targetLanguage == Language.vi ? word : null, 
                         null, 
-                        null, 
+                        audio, 
                         targetLanguage == Language.en ? word : null);
+
                     if(newOption.IsFailure) continue;
                     option = newOption.Value;
                     await _optionRepository.CreateOption(option);
