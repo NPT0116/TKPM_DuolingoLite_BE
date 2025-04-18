76e06a4a0dd31f9beb460b2e40cba9f2ad6c0885
pinkWar123 <nquan003@gmail.com>
2025-04-18 18:38:58 +0700

    DUOLITE-76 Fix: words created are not linked to questions when generating a new question with English text

--- src/Application/Features/Learning/Lessons/Commands/AddQuestions/Services/IWordGeneratorService.cs
+++ src/Application/Features/Learning/Lessons/Commands/AddQuestions/Services/IWordGeneratorService.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain.Entities.Learning.Questions;
+using Domain.Entities.Learning.Questions.QuestionOptions;
+using SharedKernel;
 
 namespace Application.Features.Learning.Lessons.Commands.AddQuestions.Services
 {
     public interface IWordGeneratorService
     {
-        Task GenerateWords(string englishText);
+        Task<Result<List<QuestionWord>>> GenerateWords(Domain.Entities.Learning.Questions.Question question, string englishText);
     }
 }
--- src/Application/Features/Learning/Lessons/Commands/AddQuestions/Services/WordGeneratorService.cs
+++ src/Application/Features/Learning/Lessons/Commands/AddQuestions/Services/WordGeneratorService.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Common.Interface;
+using Application.Features.Learning.Lessons.Queries.GetListOfLessonFromCourseId;
 using Application.Features.Media.Commands.Upload;
 using Application.Interface;
+using Domain.Entities.Learning.Questions.QuestionOptions;
 using Domain.Entities.Learning.Words;
 using Domain.Entities.Media;
 using Domain.Entities.Media.Constants;
 using Domain.Entities.Media.Enums;
 using Domain.Repositories;
 using MediatR;
+using SharedKernel;
 
 namespace Application.Features.Learning.Lessons.Commands.AddQuestions.Services
 {
@@ -30,27 +33,48 @@ namespace Application.Features.Learning.Lessons.Commands.AddQuestions.Services
             _textToSpeechService = textToSpeechService;
         }
 
-        public async Task GenerateWords(string englishText)
+        public async Task<Result<List<QuestionWord>>> GenerateWords(
+            Domain.Entities.Learning.Questions.Question question,
+            string englishText)
         {
-            if(string.IsNullOrEmpty(englishText)) return;
+            var questionWords = new List<QuestionWord>();
+            if(string.IsNullOrEmpty(englishText)) return Result.Failure<List<QuestionWord>>(QuestionWordsError.EmptyEnglishText);
 
             var words = await _wordService.SplitWordsFromString(englishText);
-            if(words.Count == 0) return;
+            if(words.Count == 0) return Result.Failure<List<QuestionWord>>(QuestionWordsError.EmptyEnglishText);
 
+            var order = 1;
             foreach (var word in words)
             {
                 var existingWord = await _wordRepository.FindWord(word);
-                if(existingWord != null) continue;
-                Console.WriteLine($"Generating word: {word}");
+                QuestionWord? questionWord = null;
+                if(existingWord != null)
+                {
+                    var createQuestionWord = QuestionWord.Create(existingWord, question, order++);
+                    if(createQuestionWord.IsFailure) return Result.Failure<List<QuestionWord>>(createQuestionWord.Error);
+                    questionWord = createQuestionWord.Value;
+                }
+                else
+                {
+                    Console.WriteLine($"Generating word: {word}");
 
-                var uploadedAudio = await _textToSpeechService.GenerateMediaFromText(word, MediaConstants.Word);
-                Domain.Entities.Media.Media? wordAudio = null;
-                if(uploadedAudio.IsSuccess) wordAudio = uploadedAudio.Value;
+                    var uploadedAudio = await _textToSpeechService.GenerateMediaFromText(word, MediaConstants.Word);
+                    Domain.Entities.Media.Media? wordAudio = null;
+                    if(uploadedAudio.IsSuccess) wordAudio = uploadedAudio.Value;
 
-                var newWord = Word.Create(word, wordAudio!);
-                if(newWord.IsSuccess) await _wordRepository.AddWord(newWord.Value);
+                    var newWord = Word.Create(word, wordAudio!);
+                    if(newWord.IsFailure) return Result.Failure<List<QuestionWord>>(newWord.Error);
+                    await _wordRepository.AddWord(newWord.Value);
 
+                    var createQuestionWord = QuestionWord.Create(newWord.Value, question, order++);
+                    if(createQuestionWord.IsFailure) return Result.Failure<List<QuestionWord>>(createQuestionWord.Error);
+                    questionWord = createQuestionWord.Value;
+                }
+
+                if(questionWord != null) questionWords.Add(questionWord);
             }
+
+            return Result.Success(questionWords);
         }
     }
 }
